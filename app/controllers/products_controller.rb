fce9aa99354a5cfb63802105fcda9118f905023c
Knight~ <x665adex563@gmail.com>
2022-03-19 12:12:50 +0800

    implement carts index function

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -11,7 +11,7 @@ class ProductsController < ApplicationController
     @product = Product.find(params[:id])
 
     current_cart.add_product_to_cart(@product)
-    flash[:notice] = "成功加入購物車"
+    flash[:notice] = "成功加入购物车"
     redirect_to :back
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,17 @@
 Rails.application.routes.draw do
-  root 'products#index'
-
-  devise_for :users
-
   namespace :admin do
     resources :products
   end
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :users
 
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'products#index'
   resources :products do
     member do
       post :add_to_cart
     end
   end
+
+  resources :carts
 end
